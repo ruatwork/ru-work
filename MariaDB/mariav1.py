10b4747adba03b87f7ea2b70ef4774a74422b6d7
Vili Viita <vili.viita1@gmail.com>
2019-02-17 17:49:18 +0200

    V1 with variables, working

--- MariaDB/mariav1.py
+++ MariaDB/mariav1.py
@@ -2,20 +2,21 @@
 
 #!/usr/bin/python3
 import mysql.connector as mariadb
-
+var1='random_variable'
+var2='random_variable2'
 # Connect to MariaDB
-mariadb_connection = mariadb.connect(host='server_ip', user='your_user', password='your_pw', database='your_database')
+mariadb_connection = mariadb.connect(host='your_ip', user='your_dbuser', password='your_pw', database='your_database')
 cursor = mariadb_connection.cursor()
 
 # Insert a new row to a table
 try:
-	cursor.execute("insert into your_table (name,address) values (%s,%s)", ('Turo Takkanen','445566'))
+	cursor.execute("insert into your_table (name,address) values (%s, %s)", (var1, var2))
 except mariadb.Error as error:
 	print("Error: {}".format(error))
 
 # Commit changes to table, autocommit is disabled by default
 mariadb_connection.commit()
-print ("The last inserted id was: "), cursor.lastrowid
+print ("The last inserted id was: "),cursor.lastrowid
 
 # Close connection to MariaDb
 mariadb_connection.close()
