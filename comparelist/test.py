c0376e4be62539a7fc55ad2ddb72b4e44f41e1a8
Jaakko Veijonen <vekkari@gmail.com>
2019-02-17 13:35:16 +0000

    list comparison done

--- comparelist/test.py
+++ comparelist/test.py
@@ -9,12 +9,19 @@ with open("checklist.txt", "r") as alltext:
 		for item in alltext]
 
 # open and make a list of the file containing current connected MAC addresses
-with open("mac.txt", "r") as crontext:
+with open("present.txt", "r") as crontext:
 	present = [item.strip()
 		for item in crontext]
 
-print checklist
-print present
+print (checklist)
+print (present)
 
 # compare checklist and present list - those in present are at work, those missing are not
 
+absent = set(checklist) - set(present)
+
+with open('absent.txt', 'w') as abstext:
+	for i in absent:
+		abstext.write("%s\n" % i)
+		print(i)
+#print (set(checklist) - set(present))
