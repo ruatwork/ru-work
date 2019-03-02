c4f5e6081285371e0d2bad19992a9dc67a248f13
roopelinux <roope.varttila@myy.haaga-helia.fi>
2019-03-02 19:26:36 +0200

    babysteps

---
+++ various_tests/timestamp/time1.py
@@ -0,0 +1,8 @@
+#/!usr/bin/python3
+
+#testing datetime since we need a timestamp later on
+
+import time
+import datetime
+
+print "Current date and time:" , datetime.datetime.now().strftime("%d.%m.%y %H:%M")
