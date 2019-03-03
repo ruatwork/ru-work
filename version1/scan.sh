0739148e22c0a5520bcaec77fddc0fd9bbd19b9b
roopelinux <roope.varttila@myy.haaga-helia.com>
2019-03-03 12:14:37 +0200

    fix scan

--- version1/scan.sh
+++ version1/scan.sh
@@ -1,6 +1,6 @@
 # Find devices connetted
 # fill your own network address and mask
-nmap -sP -n -oN scan.txt 111.222.333.444/mask &&
+nmap -sn -n -oN scan.txt 111.222.333.444/mask &&
 
 # remove everything but MAC addresses
 grep -E -o '[[:xdigit:]]{2}(:[[:xdigit:]]{2}){5}' scan.txt > present.txt &&
