641c3e5a9f50b718badbbb7b0c204eb5adc4ddc1
Vipa94 <36206447+Vipa94@users.noreply.github.com>
2019-03-08 12:49:41 +0200

    Update mariatest2.py

--- various_tests/MariaDB/mariatest2.py
+++ various_tests/MariaDB/mariatest2.py
@@ -4,19 +4,19 @@
 import mysql.connector as mariadb
 
 # Connect to MariaDB
-mariadb_connection = mariadb.connect(host='188.166.77.172', user='admin', password='TeroNot@Toissa', database='testi')
+mariadb_connection = mariadb.connect(host='your_ip', user='your_user', password='your_pw', database='your_pw')
 cursor = mariadb_connection.cursor()
 
 # Retrieve information from database
 some_name = 'Seppo Valimaki'
-cursor.execute("select name,address from testi2 where name=%s", (some_name,))
+cursor.execute("select name,address from your_table where name=%s", (some_name,))
 
 for name, price in cursor:
 	print("Name: {}, Address: {}").format(name,address)
 
 # Insert a new row to a table
 try:
-	cursor.execute("insert into testi2 (name,address) values (%s,%s)", ('Antero Vipunen','334455'))
+	cursor.execute("insert into your_table (name,address) values (%s,%s)", ('Antero Vipunen','334455'))
 except mariadb.Error as error:
 	print("Error: {}".format(error))
 
