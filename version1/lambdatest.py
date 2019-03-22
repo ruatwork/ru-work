04c16a49bd9dc7b8a4c6c31c738faf76a348946f
Vili Viita <vili.viita1@gmail.com>
2019-03-22 10:53:54 +0000

    no workings

--- version1/lambdatest.py
+++ version1/lambdatest.py
@@ -12,8 +12,13 @@ with open("present.txt", "r") as presenttext:
 		for item in presenttext]
 
 while (currentrow <= lastrow -1):
+
 	if any(map(lambda each: each in checklist[currentrow], presentlist)):
 		print (currentrow)
 		print (checklist[currentrow]) #print the item from the list that is in the current position (0,1,2,3,4)
 		print ("present")
 		currentrow += 1
+
+
+	else:
+		print ("BorkBorkEn")
